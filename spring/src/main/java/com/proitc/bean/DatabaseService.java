09f55f381e49cc811a6e2b2d2f39ec4eed219d8c
Juan Moreno <earth001@gmail.com>
2018-08-13 14:12:12 -0300

    Added log config

--- spring/src/main/java/com/proitc/bean/DatabaseService.java
+++ spring/src/main/java/com/proitc/bean/DatabaseService.java
@@ -2,7 +2,8 @@ package com.proitc.bean;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class DatabaseService {
 
+  private static final Logger log = LoggerFactory.getLogger(DatabaseService.class.getName());
+
   @Autowired
   public DatabaseService(DataSource dataSource) {
-    System.out.println("Datasource through Constructor injection in DatabaseService");
+    log.debug("Datasource through Constructor injection in DatabaseService");
     System.out.println("DataSource: " + dataSource.getDriverClassName());
     System.out.println("DataSource: " + dataSource.getUrl());
   }
